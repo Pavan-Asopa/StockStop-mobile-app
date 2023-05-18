7d0ee3c2d1014b3ad1721a28ee4dd46a7e67ba4a
Pavan Asopa <pavanasopa14@gmail.com>
2023-05-18 18:44:19 +1000

    searchscreen_update

---
+++ components/SearchList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard, Text, FlatList } from 'react-native';
+import { List, Searchbar } from 'react-native-paper';
+
+
+export default function SearchList({stocks}) {
+
+
+
+    return (
+      <View>
+        {stocks.map((item) =>
+          <List.Item
+            title={item.name}
+             titleStyle={styles.title}
+            left={props => <List.Icon {...props} color={"#2b6777"} icon="arrow-right-drop-circle-outline"/>}
+            //onPress={() => }
+          />
+        )}
+      </View>
+    );
+  }
+
+
+  const styles = StyleSheet.create({
+    title: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+  // use scaleSize(x) to adjust sizes for small/large screens
+  });
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,71 +1,75 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, ScrollView, View, TouchableWithoutFeedback, Keyboard, Text, FlatList } from 'react-native';
-import { List, Colors } from 'react-native-paper';
+import { Searchbar } from 'react-native-paper';
 import { useStocksContext } from '../contexts/StocksContext';
 import { scaleSize } from '../constants/Layout';
 import { Ionicons } from '@expo/vector-icons';
 
-// function searchBar() {
+import SearchList from '../components/SearchList';
+
+// export function SearchBar(state){
 
 // }
 
-function SearchList() {
-  const stocks = [
-    {
-      symbol: 'NFLX',
-      key: 'NFLX',
-      name: 'Netflix'
-    },
-    {
-      symbol: 'SPOT',
-      key: 'SPOT',
-      name: 'Spotify'
-    },
-    {
-      symbol: 'TSLA',
-      key: 'TSLA',
-      name: 'Tesla'
-    }
-  ];
-
-  return (
-    <View style={styles.container}>
-      {stocks.map((item) =>
-        <List.Item
-          title={item.name}
-          titleStyle={styles.title}
-          left={props => <List.Icon {...props} color={"#2b6777"} icon="arrow-right-drop-circle-outline"/>}
-          //onPress={() => }
-        />
-      )}
-      {/* <FlatList
-        data = {stocks}
-        renderItem = {({item}) => 
-          <Text style={styles.stockItems}>
-            <Text style={styles.stockSymbol}>
-              {item.symbol}::
-            </Text>
-            <Text style={styles.stockName}>
-              {item.name}
-            </Text>
-          </Text>}
-      /> */}
-    </View>
-  );
-}
+// const searchFilterFunction = (props) => {
+//   // Check if searched text is not blank
+//   if (props.searchText) {
+//     // Inserted text is not blank
+//     // Filter the masterDataSource
+//     // Update FilteredDataSource
+//     const newData = masterDataSource.filter(
+//       function (item) {
+//         const itemData = item.title
+//           ? item.title.toUpperCase()
+//           : ''.toUpperCase();
+//         const textData = text.toUpperCase();
+//         return itemData.indexOf(textData) > -1;
+//     });
+//     setFilteredDataSource(newData);
+//     setSearch(props.searchText);
+//   } else {
+//     // Inserted text is blank
+//     // Update FilteredDataSource with masterDataSource
+//     setFilteredDataSource(props.fullList);
+//     setSearch(props.searchText);
+//   }
+// };
 
 export default function SearchScreen({ navigation }) {
   //const { ServerURL, addToWatchlist } = useStocksContext();
-  const [state, setState] = useState([]);
+  const [fullList, setFullList] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [filteredList, setFilteredList] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // const searchFilterFunction = (searchText) => {
+  //   // Check if searched text is not blank
+  //   if (searchText) {
+  //     // Inserted text is not blank
+  //     // Filter the masterDataSource
+  //     // Update FilteredDataSource
+  //     const newData = fullList.filter((name) => 
+  //       name.includes('searchText')
+  //     )
+        
+  //     setFilteredDataSource(newData);
+  //     setSearch(searchText);
+  //   } else {
+  //     // Inserted text is blank
+  //     // Update FilteredDataSource with masterDataSource
+  //     setFilteredList(fullList);
+  //     setSearch(searchText);
+  //   }
+  // };
+
+
 
   useEffect(() => {
     fetch(`https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=02ea7babe095fdebdb6c4ef948886e07`)
       .then(res => res.json())
       .then(data => 
         data.map(stock => {
-          console.log(stock.name);
           return {
             name: stock.name,
             symbol: stock.symbol
@@ -73,30 +77,49 @@ export default function SearchScreen({ navigation }) {
         }))
       .catch((error) => setError(error.message))
       .finally(() => setLoading(false))
-    .then(stocks => setState(stocks))
+    .then(stocks => setFullList(stocks))
   }, []);
 
-  // if (loading) {
-  //   return (
-  //     <Text>
-  //       Loading available stocks...
-  //     </Text>
-  //   );
-  // }
 
-  // if (error) {
-  //   return (
-  //     <Text>
-  //       Something went wrong: {error}
-  //     </Text>
-  //   );
-  // }
+
+  if (loading) {
+    return (
+      <Text>
+        Loading available stocks...
+      </Text>
+    );
+  }
+
+  if (error) {
+    return (
+      <Text>
+        Something went wrong: {error}
+      </Text>
+    );
+  }
+
+  let test = "la";
+ // test = test[0].toUpperCase()+test.slice(1,-1).toLowerCase();
+
+
+  const newData = fullList.filter((stock) => 
+  stock.name.includes(test[0].toUpperCase()+test.slice(1,-1).toLowerCase())
+
+)
+console.log(newData);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <SearchList />
-      </View>
+      <ScrollView>
+      <Searchbar
+      placeholder="Search"
+      //onChangeText={onChangeSearch}
+      //value={searchQuery}
+    />
+      <ScrollView style={styles.container}>
+        <SearchList stocks = {fullList} />
+      </ScrollView>
+      </ScrollView>
     </TouchableWithoutFeedback>    
   );
 }
@@ -106,14 +129,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000'
   },
-  title: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  description: {
-    color: '#fff',
-    fontSize: 18,
-  }
 // use scaleSize(x) to adjust sizes for small/large screens
 });
