fef3a77b42366cfe17bb8c5f480c1865f3fbf636
Pavan Asopa <pavanasopa14@gmail.com>
2023-05-28 18:00:09 +1000

    express

---
+++ server/stockstop-backend/knexfile.js
@@ -0,0 +1,9 @@
+module.exports =  {
+    client: 'mysql2',
+    connection:  {
+      host: 'localhost',
+      database: 'stockstopdb',
+      user: 'root',
+      password: 'welcome123'
+    }
+};
